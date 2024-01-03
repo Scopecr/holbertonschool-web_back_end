7aecb0686aa25e407ef9a8383ed314bafe6700ba
Scopecr <wjrcarrion@gmail.com>
2024-01-03 18:51:36 -0400

    9 element

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Annotations for all parameters and the return value
+"""
+
+
+from typing import Callable
+from typing import Iterable
+from typing import List
+from typing import Sequence
+from typing import Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns list of tuples, one for each element of lst, of the form (i, len(i))"""
+    return [(i, len(i)) for i in lst]
