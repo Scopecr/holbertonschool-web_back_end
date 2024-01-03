8a944ad2777eec8295c4617428dbb57f1fad85b3
Scopecr <wjrcarrion@gmail.com>
2024-01-03 18:46:04 -0400

    make multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Make a type-annotated function make_multiplier that takes a float multiplier as
+"""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """returns a function that multiplies a float by multiplier."""
+    def multiply(n: float):
+        return n * multiplier
+    return multiply
