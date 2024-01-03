3aa7b962cac9cbea5df014c53c086faf7ec0a0f9
Scopecr <wjrcarrion@gmail.com>
2024-01-03 18:46:52 -0400

    pycodestyle 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,7 @@ Make a type-annotated function make_multiplier that takes a float multiplier as
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier."""
     def multiply(n: float):
