98e02af652bba2ab97ca1a8e687967045d35984c
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-04 13:48:52 -0400

    1-concurrent

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,14 +4,17 @@ Basic Async Syntax
 """
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
         n: number of times wait_random will be called
         max_delay: max wait time for each call of wait_random
     """
-    delays = []
-    for i in range(n):
-        delays.append(await wait_random(max_delay))
-    return sorted(delays)
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delay_list = []
+    for future in asyncio.as_completed(tasks):
+        result = await future
+        delay_list.append(result)
+    return delay_list
