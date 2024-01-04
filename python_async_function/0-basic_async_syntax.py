29a16ac7c1f61eecdf5608cc36684a2c94e26b96
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-04 13:16:20 -0400

    readme and 0 basic

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Basic Async Syntax
+"""
+
+
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Async routine that takes in an integer argument
+    """
+    random_float = __import__('random').uniform(0, max_delay)
+    await asyncio.sleep(random_float)
+    return random_float
