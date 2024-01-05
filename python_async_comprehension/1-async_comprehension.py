d64edff9756408f323ffbbf724088fcb9b2eb595
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-05 15:18:30 -0400

    pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Import sarync_generator and  write a coroutine called async_comprehension that takes no arguments
-The coroutine will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.
+The coroutine will collect 10 random numbers using an async comprehensing over async_generator, 
+then return the 10 random numbers.
 """
 
 from typing import Lists
