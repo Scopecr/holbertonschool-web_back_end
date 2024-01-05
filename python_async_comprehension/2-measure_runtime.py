07c222859b62a9eff120f8da47436461e706a69e
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-05 15:22:16 -0400

    2- mesure

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a measure_runtime coroutine that will execute
+async_comprehension four times in parallel using asyncio.gather."""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Return the total runtime."""
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
