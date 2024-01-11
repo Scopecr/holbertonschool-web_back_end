154be927eca9451cacf2224855b14af21955130a
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-11 06:43:58 -0400

    10 update topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/binenv python3
+"""Python code that changes topics of a school document"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Python code that changes topics of a school document"""
+    return mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
