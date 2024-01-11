374fcb73a09056c3a38d6a0ea861f89b83122be5
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-11 14:19:24 -0400

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-"""log stats"""
+"""Log stats"""
 from pymongo import MongoClient
+
 
 
 client = MongoClient('mongodb://localhost:27017/')
 
 
+
 db = client.logs
-collection = db.ngnix
+collection = db.nginx
+
 
 total_logs = collection.count_documents({})
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {method: collection.count_documents({"method": method}) for method in methods}
 
-status_count = collection.count_documents({"method": 'GET', "path": "/status"})
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {method: collection.count_documents({'method': method}) for method in methods}
+
+
+status_count = collection.count_documents({'method': 'GET', 'path': '/status'})
 
 
 print(f"{total_logs} logs")
