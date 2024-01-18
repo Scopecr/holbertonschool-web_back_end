34e14f1c2cc4eb4f8d6c71ba20a1ffb39ac7c742
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-17 20:17:02 -0400

    fixed some bugs that prevented code to run propperly

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -8,8 +8,8 @@ export function getLast() {
 }
 
 export function teaskNext() {
-    let combination  = 'Buet sometimes let';
-    combination = getLast();
+    let combination  = 'But sometimes let';
+    combination += getLast();
 
     return combination;
 }
