2b57f8e9a61296960edcd0c313602afed7c052a4
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-18 08:22:17 -0400

    added const self to see if this is correct output

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,5 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA, "Union Square'];
+
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighbothood);
     return this.sanFranciscoNeighborhoodsl;
