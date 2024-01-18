40ff8da1c2d866e4d2c98cbc2778d80b78ffcd86
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-18 08:03:08 -0400

    arrow function re written

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,7 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-    this.addneighborhood = (newNeiighborhood) => {
-        this.sanFranciscoNeighborhoods.push(newNeiighborhood);
-        return this.sanFranciscoNeighborhoods;
-    };
+  this.sanFranciscoNeighborhoods = ['SOMA, "Union Square'];
+  this.addNeighborhoood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighbothood);
+    return this.sanFranciscoNeighborhoodsl;
+  };
 }
