7951df4b70408a7d7fc7c1732bd88d575b631fad
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-17 20:29:55 -0400

    1-2 created

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,3 +1,8 @@
-export const getNeighborhoodsList() {
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+    this.addneighborhood = (newNeiighborhood) => {
+        this.sanFranciscoNeighborhoods.push(newNeiighborhood);
+        return this.sanFranciscoNeighborhoods;
+    };
 }
