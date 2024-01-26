a6524e6fbfd87a4fb41026cfa3ccb6365a2a5402
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-26 17:13:31 -0400

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,19 +1,33 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
+  };
+
   try {
-    const results = await Promise.allSettled([signUpUser(firstName, lastName),
-      uploadPhoto(fileName)]);
-    return results.map((result) => ({
-      status: result.status,
-      // Check if the promise was fulfilled or rejected and format the value accordingly
-      value: result.status === 'fulfilled'
-        ? result.value
-        : `Error: ${result.reason.message}`, // Prepend "Error: " to the error message
-    }));
-  } catch (error) {
-    console.error('An error occurred:', error);
-    throw error; // Re-throw the error if you want calling code to handle it
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
   }
+
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;
