4e72180499da36f7d3a59f6579e036b6cf143a65
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-29 17:33:18 -0400

    added comment documentation

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,6 @@
+// A function named updateStudentGradeByCity that returns
+// an array of students for a specific city with their new grade.
+
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
