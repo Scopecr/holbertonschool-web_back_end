65881e5171494a6fbe62c5680e08f9a139e5f498
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2024-01-31 13:09:33 -0400

    e fixed potentially

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,3 @@
 export default function getStudentIdsSum(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.reduce((accumulator, student) => accumulator + student.id, 0);
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
